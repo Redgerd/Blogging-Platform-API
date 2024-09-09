27bc8715c7160aa9242ee633c30dbd28707e0c0e
Redgerd <msalaarm@gmai.com.com>
2024-09-09 20:58:07 +0500

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 require("dotenv/config");
+const app = express();
+
+app.use(express.json()); // to parse JSON body
+
+// Routes for CRUD Operations
+
+// Database Setup
+const connectDB = require("./db");
 
 app.listen(PORT, (error) => {
   if (!error) console.log("Listening on port " + PORT);
